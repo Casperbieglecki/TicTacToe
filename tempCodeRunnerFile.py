0f4eb3827a3a826bbbf45d503261f42993e11572
unknown <Casperb10@icloud.com>
2024-05-25 23:38:10 -0400

    Added a win comment

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,25 +1,4 @@
-#Proj 1
-import tkinter as tk
-from tkinter import messagebox
-
-
-def Game():
-    root = tk.Tk()
-    root.geometry('500x500')
-    root.title('Tic Tac Toe')
-
-    #create grid
-    root.columnconfigure(0, weight=1)
-    root.columnconfigure(1, weight=1)
-    root.columnconfigure(2, weight=1)
-    root.rowconfigure(0, weight=1)
-    root.rowconfigure(1, weight=1)
-    root.rowconfigure(2, weight=3)
-    root.rowconfigure(3, weight=3)
-    turn_track = 0
- 
-    def winner():
-        #Determine Win
+def winner():
         if but1['text'] == but2['text'] == but3['text'] != '':
             but1.configure(bg = 'red')
             but2.configure(bg = 'red')
@@ -65,54 +44,3 @@ def Game():
         
         else:
             pass
-
-
-    def onClick(but):
-        nonlocal turn_track
-        turn_track += 1
-        if turn_track%2 == 0:
-            but.configure(text = 'x', state = 'disabled')
-            winner()
-        else:
-            but.configure(text = 'o', state = 'disabled')
-            winner()
-
-
-    titlelabel = tk.Label(root, text = 'Tic Tac Toe')
-    titlelabel.grid(row=0,column = 1)
-
-#Top row
-    but1 = tk.Button(root, text = '', command = lambda: onClick(but1))
-    but1.grid(row=1, column=0, sticky='nswe')
-
-    but2 = tk.Button(root, text='', command = lambda: onClick(but2))
-    but2.grid(row=1, column=1, sticky='nswe')
-
-    but3 = tk.Button(root, text='', command = lambda: onClick(but3))
-    but3.grid(row=1, column=2, sticky='nswe')
-
-#Middle row
-    but4 = tk.Button(root, text='', command = lambda: onClick(but4))
-    but4.grid(row=2, column=0, sticky='nswe')
-    
-    but5 = tk.Button(root, text='', command = lambda: onClick(but5))
-    but5.grid(row=2, column=1, sticky='nswe')
-
-    but6 = tk.Button(root, text='', command = lambda: onClick(but6))
-    but6.grid(row=2, column=2, sticky='nswe')
-
-#Bottom row
-    but7 = tk.Button(root, text='', command = lambda: onClick(but7))
-    but7.grid(row=3, column=0, sticky='nswe')
-    
-    but8 = tk.Button(root, text='', command = lambda: onClick(but8))
-    but8.grid(row=3, column=1, sticky='nswe')
-
-    but9 = tk.Button(root, text='', command = lambda: onClick(but9))
-    but9.grid(row=3, column=2, sticky='nswe')
-
-
-    root.mainloop()
-
-
-Game()
